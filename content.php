19d8bd4868ab1342c53eecfd566912b0de012264
chainwood <chainwood@chain>
2017-07-03 16:07:35 +0800

    文章单页（数据逻辑，不含UI布局）

---
+++ content.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage ZBPress
+ * @since ZBPress 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" class="single-article">
+	<?php
+		// Post thumbnail.
+		ZBPress_post_thumbnail();
+	?>
+
+	<header class="entry-header">
+		<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+			endif;
+		?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading %s', 'ZBPress' ),
+				the_title( '<span class="screen-reader-text">', '</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'ZBPress' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'ZBPress' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php
+		// Author bio.
+		if ( is_single() && get_the_author_meta( 'description' ) ) :
+			get_template_part( 'author-bio' );
+		endif;
+	?>
+
+	<footer class="entry-footer">
+		<?php ZBPress_entry_meta(); ?>
+		<?php edit_post_link( __( 'Edit', 'ZBPress' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
--- functions.php
+++ functions.php
@@ -15,84 +15,49 @@ function catch_the_image() {
     return $first_img;
 }
 
-define('SETTING_ID_BEIAN_ICP', 'zh_cn_l10n_icp_num');
-define('SETTING_ID_BEIAN_GONGAN', 'zh_cn_l10n_gongan_num');
+// define('SETTING_ID_BEIAN_ICP', 'zh_cn_l10n_icp_num');
+// define('SETTING_ID_BEIAN_GONGAN', 'zh_cn_l10n_gongan_num');
 
-// 增加主题选项
-add_action( 'customize_register', 'zbu_customize_register' );
-function zbu_customize_register( $wp_customize ) {    
+// // 增加主题选项
+// add_action( 'customize_register', 'zbu_customize_register' );
+// function zbu_customize_register( $wp_customize ) {    
 
-	// ICP 备案信息
-    $wp_customize->add_setting( SETTING_ID_BEIAN_ICP, array(
-        'default'        => '浙ICP备0000000号',
-        'type'           => 'option',
-        'capability'     => 'edit_theme_options',
-        'transport'      => 'refresh'
-    ) );
+// 	// ICP 备案信息
+//     $wp_customize->add_setting( SETTING_ID_BEIAN_ICP, array(
+//         'default'        => '浙ICP备0000000号',
+//         'type'           => 'option',
+//         'capability'     => 'edit_theme_options',
+//         'transport'      => 'refresh'
+//     ) );
 
-    $wp_customize->add_control( new WP_Customize_Control( 
-    	$wp_customize, SETTING_ID_BEIAN_ICP, array(
-        'label'      => __( 'ICP备案号', 'onepress-zongbu' ),
-        'section'    => 'title_tagline',
-        'settings'   => SETTING_ID_BEIAN_ICP,
-    ) ) );
+//     $wp_customize->add_control( new WP_Customize_Control( 
+//     	$wp_customize, SETTING_ID_BEIAN_ICP, array(
+//         'label'      => __( 'ICP备案号', 'onepress-zongbu' ),
+//         'section'    => 'title_tagline',
+//         'settings'   => SETTING_ID_BEIAN_ICP,
+//     ) ) );
 
-    // 公安备案信息
-    $wp_customize->add_setting( SETTING_ID_BEIAN_GONGAN, array(
-        'default'        => '浙公网安备00000000000000号',
-        'type'           => 'option',
-        'capability'     => 'edit_theme_options',
-        'transport'      => 'refresh'
-    ) );
+//     // 公安备案信息
+//     $wp_customize->add_setting( SETTING_ID_BEIAN_GONGAN, array(
+//         'default'        => '浙公网安备00000000000000号',
+//         'type'           => 'option',
+//         'capability'     => 'edit_theme_options',
+//         'transport'      => 'refresh'
+//     ) );
 
-    $wp_customize->add_control( new WP_Customize_Control( 
-    	$wp_customize, SETTING_ID_BEIAN_GONGAN, array(
-        'label'      => __( '公安备案号', 'onepress-zongbu' ),
-        'section'    => 'title_tagline',
-        'settings'   => SETTING_ID_BEIAN_GONGAN,
-    ) ) );
+//     $wp_customize->add_control( new WP_Customize_Control( 
+//     	$wp_customize, SETTING_ID_BEIAN_GONGAN, array(
+//         'label'      => __( '公安备案号', 'onepress-zongbu' ),
+//         'section'    => 'title_tagline',
+//         'settings'   => SETTING_ID_BEIAN_GONGAN,
+//     ) ) );
 
-}
+// }
 
-// 增加备案信息
-function add_icp_num($wp_customize)  {  
-	$beian_icp = get_option( SETTING_ID_BEIAN_ICP );
-	$beian_gongan = get_option( SETTING_ID_BEIAN_GONGAN );
-	echo 'Copyright &copy; ' . date('Y') . ' ' . get_bloginfo();
-	echo ' - Powered by <a href="http://www.zongbutech.com">宗布科技</a><br>';
-    echo '<a href="http://www.miitbeian.gov.cn/" rel="external nofollow" target="_blank">' . $beian_icp . '</a>';
-    echo ' <img src="http://www.beian.gov.cn/img/ghs.png"> ';
-    echo '<a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=' . $beian_gongan . '">' . $beian_gongan . '</a>';
-}
-add_action('onepress_footer_site_info', 'add_icp_num', 20); 
-function remove_origin_siteinfo() {
-	remove_action( 'onepress_footer_site_info', 'onepress_footer_site_info' );
-}
-add_action('init', 'remove_origin_siteinfo');
-
-// 登录表单shortcode
-function zbu_login_form_shortcode() {
-    if ( is_user_logged_in() )
-        return '';
-    return wp_login_form( array( 'echo' => false ) );
-}
-// 重定向简码
-function zbu_redirect_shortcode($params) {
-    extract(shortcode_atts(array(
-	'url' => '/',
-	), $params));
-
-    header('Location: '.$url);
-}
-// 注册简码
-function zbu_add_shortcodes() {
-    add_shortcode( 'zbu-login-form', 'zbu_login_form_shortcode' );
-    add_shortcode( 'zbu-redirect', 'zbu_redirect_shortcode' );
-}
-add_action( 'init', 'zbu_add_shortcodes' );
-
-// 隐藏工具条
-if ( !is_admin() ) {  
-    add_filter('show_admin_bar', '__return_false'); 
-}
+/**
+ * Custom template tags for this theme.
+ *
+ * @since ZBPress 1.0
+ */
+require get_template_directory() . '/inc/template-tags.php';
 ?>
